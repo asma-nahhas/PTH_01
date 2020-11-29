2793bd8aaed6d8795a015a180aa3a1ce2c01dea2
unknown <asmanahasfree2@gmail.com>
2020-11-29 20:11:49 +0200

    Last Update

--- myapp/views.py
+++ myapp/views.py
@@ -11,109 +11,101 @@ import re
 from Tables.models import Table
 
 
-#Define Home Page view 
+# Define Home Page view
 
-def  Home(request):
-    tables=Table.objects.all()
-    context={"tables":tables}
-    return render(request,"Home.html",context)
-
-
+def Home(request):
+    tables = Table.objects.all()
+    context = {"tables": tables}
+    return render(request, "Home.html", context)
 
 
-def  editTable(request):
-    tables=Table.objects.all()
-    message=""
-    tblName =  request.POST["editTableName"]
-    index =  request.POST["editId"]
+def editTable(request):
+    tables = Table.objects.all()
+    message = ""
+    tblName = request.POST["editTableName"]
+    index = request.POST["editId"]
     print("The Table Name is : ")
     print(tblName)
     # Defining The Regular Expression of paraghraphs strings
     pattern = re.compile(r'^Table+[0-9]+$')
 
     if re.match(pattern, tblName):
-        message="Saved Successfuly"
+        message = "Saved Successfuly"
         Table.objects.filter(id=index).update(name=tblName)
 
     else:
-        message="Table Name should be like Table0"
+        message = "Table Name should be like Table0"
+
+    context = {"tables": tables, "message": message}
+    return render(request, "Home.html", context)
 
 
-
-    context={"tables":tables,"message":message}
-    return render(request,"Home.html",context)
-
-
-#Upload the Document file and save a copy to the database model Table
+# Upload the Document file and save a copy to the database model Table
 
 def uploadDocx(request):
-    message =""
+    message = ""
     # Handle file upload
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
             newdoc = Document(docfile=request.FILES['docfile'])
-            #newdoc.docfile.name='Table1.docx'
+            # newdoc.docfile.name='Table1.docx'
             newdoc.save()
             print(newdoc.docfile.name)
 
-            #we should save all the Tables Paragraphs as table header
-            Headers=[]
-            f = open('media/'+newdoc.docfile.name, 'rb')
-            doc=DOCX(f)
+            # we should save all the Tables Paragraphs as table header
+            Headers = []
+            f = open('media/' + newdoc.docfile.name, 'rb')
+            doc = DOCX(f)
 
-            fileIndexVar=0
+            fileIndexVar = 0
 
             # Defining The Regular Expression of paraghraphs strings
             pattern = re.compile(r'^Table+[0-9]+$')
 
-
-            #get All the pre added tables' headers
-            allHeaders=[]
-            allHeaders=Table.objects.values_list('name', flat=True)
+            # get All the pre added tables' headers
+            allHeaders = []
+            allHeaders = Table.objects.values_list('name', flat=True)
 
             print("All Headers Are")
             print(allHeaders)
 
             print("All the Paraghraphs")
 
-            allCurrentParags=[]
+            allCurrentParags = []
             for paragraph in doc.paragraphs:
-                if paragraph.style.name=='Heading 1':
+                if paragraph.style.name == 'Heading 1':
                     allCurrentParags.append(paragraph.text)
 
             print(allCurrentParags)
 
-            for i,paragraph in enumerate(doc.paragraphs):
-                if paragraph.style.name=='Heading 1':
-                    
+            for i, paragraph in enumerate(doc.paragraphs):
+                if paragraph.style.name == 'Heading 1':
+
                     if re.match(pattern, paragraph.text):
 
-                        print("Match found the table name"+paragraph.text)
+                        print("Match found the table name" + paragraph.text)
                         message = 'File Matching the constraints Successfuly'
 
-                        if paragraph.text in allHeaders or  allCurrentParags.count(paragraph.text ) > 1  :
-                            message='The Titles in this File is not Unique with previously added Titles'
+                        if paragraph.text in allHeaders or allCurrentParags.count(paragraph.text) > 1:
+                            message = 'The Titles in this File is not Unique with previously added Titles'
                         else:
                             Headers.append(paragraph.text)
-                            #Here We Should add this table title to the table Table
-                            newTable=Table(name=paragraph.text,fileName=newdoc.docfile.name,fileIndex=fileIndexVar)
+                            # Here We Should add this table title to the table Table
+                            newTable = Table(name=paragraph.text, fileName=newdoc.docfile.name, fileIndex=fileIndexVar)
                             newTable.save();
-                            fileIndexVar+=1;
+                            fileIndexVar += 1;
 
                     else:
                         print("Match not found")
                         message = 'File headers mismatching the constraints'
 
-
-
-            print (Headers)
-
+            print(Headers)
 
             documents = Document.objects.all()
 
             # Redirect to the document list after POST
-            return render(request,'list.html',{'documents': documents,'form': form,'message':message})
+            return render(request, 'list.html', {'documents': documents, 'form': form, 'message': message})
         else:
             message = 'The form is not valid. Fix the following error:'
     else:
@@ -127,84 +119,71 @@ def uploadDocx(request):
     return render(request, 'list.html', context)
 
 
+# Convert The selected table to a json object
 
-
-
-
-  #Convert The selected table to a json object   
-
-def  JsonTable(request):
-
-    tblName =  request.POST["tblName"]
+def JsonTable(request):
+    tblName = request.POST["tblName"]
     print("The Table Name is : ")
     print(tblName)
-
 
     docFileName = Table.objects.filter(name=tblName)
 
     print(docFileName[0].fileName)
 
-    print("Table Index is: "+ str(docFileName[0].fileIndex))
+    print("Table Index is: " + str(docFileName[0].fileIndex))
 
-    #first we should get all the tables titles
-    doc=DOCX('media/'+docFileName[0].fileName)
+    # first we should get all the tables titles
+    doc = DOCX('media/' + docFileName[0].fileName)
 
-    #doc=DOCX('media/documents/demo.docx')
+    # doc=DOCX('media/documents/demo.docx')
 
     tables = doc.tables
-    
-    table=doc.tables[docFileName[0].fileIndex]
+
+    table = doc.tables[docFileName[0].fileIndex]
 
     data = []
-    title={}
-    title["title"]=tblName
-
+    title = {}
+    title["title"] = tblName
 
     data.append(title)
-    rows={}
-    
-    all_data=[]
-    #print(list(enumerate(table.rows)))
+    rows = {}
+
+    all_data = []
+    # print(list(enumerate(table.rows)))
 
     keys = None
     for i, row in enumerate(table.rows):
 
         text = (cell.text for cell in row.cells)
 
-
         # Establish the mapping based on the first row
         # headers; these will become the keys of our dictionary
         if i == 0:
-            
-            
             keys = tuple(text)
-            
+
             continue
 
         # Construct a dictionary for this row, mapping
         # keys to values for this row
         row_data = dict(zip(keys, text))
         all_data.append(row_data)
-    
-    rows["rows"]=all_data
+
+    rows["rows"] = all_data
     data.append(rows)
-        
 
     print(data)
 
-    #Convert dictionary object to Json Object
-    json_object = json.dumps(data, indent = 4)   
+    # Convert dictionary object to Json Object
+    json_object = json.dumps(data, indent=4)
 
-    #doc1 = Document.objects.filter(id=32)
-    return render(request, 'demo.html', {"json":json_object})
+    # doc1 = Document.objects.filter(id=32)
+    return render(request, 'demo.html', {"json": json_object})
 
 
+# convert the selected table to a XML Object
 
-#convert the selected table to a XML Object
-
-def  XMLTable(request):
-
-    tblName =  request.POST["tblName"]
+def XMLTable(request):
+    tblName = request.POST["tblName"]
     print("The Table Name is : ")
     print(tblName)
 
@@ -212,51 +191,46 @@ def  XMLTable(request):
 
     print(docFileName[0].fileName)
 
-    doc=DOCX('media/'+docFileName[0].fileName)
+    doc = DOCX('media/' + docFileName[0].fileName)
     tables = doc.tables
     print(tables)
-    table=doc.tables[docFileName[0].fileIndex]
-
+    table = doc.tables[docFileName[0].fileIndex]
 
     data = []
-  
-    title={}
-    title["title"]=tblName
+
+    title = {}
+    title["title"] = tblName
 
     data.append(title)
-    rows={}
-    
-    all_data=[]
-    #print(list(enumerate(table.rows)))
+    rows = {}
+
+    all_data = []
+    # print(list(enumerate(table.rows)))
 
     keys = None
     for i, row in enumerate(table.rows):
 
         text = (cell.text for cell in row.cells)
 
-
         # Establish the mapping based on the first row
         # headers; these will become the keys of our dictionary
         if i == 0:
-            
-            
             keys = tuple(text)
-            
+
             continue
 
         # Construct a dictionary for this row, mapping
         # keys to values for this row
         row_data = dict(zip(keys, text))
         all_data.append(row_data)
-    
-    rows["row"]=all_data
-    data.append(rows)
 
+    rows["row"] = all_data
+    data.append(rows)
 
     print(data)
 
-    #Convert the file to XML Object 
-    xml = dict2xml(data) 
+    # Convert the file to XML Object
+    xml = dict2xml(data)
     print(xml)
 
-    return render(request, 'xml.html', {"xml":xml})
+    return render(request, 'xml.html', {"xml": xml})
